ac2634bb49b2540d49ce999b5f9d6661516f537f
talllguy <elliott.plack@gmail.com>
2013-11-14 20:01:21 -0500

    rewrote switch
    to clarify operations, changed up the switch to allow conducting each part separately.

--- Lab11Recursion.cpp
+++ Lab11Recursion.cpp
@@ -5,51 +5,49 @@ Elliott Plack
 ************************************************************/
 
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 double loopSimple (double,int); // simple loop function
 double loopRecursion (double,int); // loop using recursion
 double loopRecursLog (double,int); // loop using recursion and log n
-double loopExponential (int); // loop using o(2^n) which may take a double time to run.
+double loopExponential (int); // loop using o(2^n) which may take a long time to run.
 
 int main ()
 {
-	double x = 0, result1 = 0, result2 = 0, result3 = 0, result4 = 0;
-	int choice = 0, n = 0; // choice for case
+	double x = 0, simpleResult = 0, recursiveResult = 0, recLogNResult = 0, exponentialResult = 0;
+	int n = 0, choice = 0;
 
-	//input
-	cout << "Enter a value for x and n to process big O notation for. \n";
-	cout << "Enter x: ";
-	cin >> x;
-	cout << "Enter n: ";
-	cin >> n;
-
-	// switch for algorithm choice
-	cout << "Type the number corresponding to the function you wish to perform below:\n"
-		 << "1. A function that uses a loop to calculate x^n in O(n)\n"
-		 << "2. A function that recursively calculates x^n in O(n)\n"
-		 << "3. A function that recursively calculates x^n in O(log n)\n"
-		 << "4. A function that has O(2^n) time complexity.\n";
+	cout << "To process big O notation for x and n: press 1\n"
+		<< "To illustrate time complexity of 2^n  : press 2\n";
 	cin >> choice;
 
 	switch (choice)
 	{
 	case 1:
-		result1 = loopSimple(x, n);
-		cout << result1;
+		cout << "Enter a value for x and n to process big O notation for. \n";
+		cout << "Enter x: ";
+		cin >> x;
+		cout << "Enter n: ";
+		cin >> n;
+
+		simpleResult = loopSimple(x, n);
+		recursiveResult = loopRecursion(x, n);
+		recLogNResult = loopRecursLog(x, n);
+
+		cout << "This program :\n"
+			<< "1. Using a loop to calculate x^n yields ........... " << simpleResult << endl
+			<< "2. Recursively calculating x^n yields ............. " << recursiveResult << endl
+			<< "3. Recursively calculating x^n using logn yields .. " << recLogNResult << endl;
 		break;
 	case 2:
-		result2 = loopRecursion(x, n);
-		cout << result2;
-		break;
-	case 3:
-		result3 = loopRecursLog(x, n);
-		cout << result3;
-		break;
-	case 4:
-		result4 = loopExponential(n);
-		cout << result4;
+		cout << "Enter a value for n to test time complexity for 2^n: ";
+		cin >> n;
+
+		exponentialResult = loopExponential(n);
+
+		cout << "4. Calculating (2^n) in big O notation yields ..... " << exponentialResult;
 		break;
 	default:
 		break;
@@ -94,7 +92,7 @@ double loopExponential (int n)
 	{
 		power = power * 2;
 		for(int k=1; k<= power; k++)
-			cout << k << endl;
+			return k;
 	}
-	return 0;
+	
 }
